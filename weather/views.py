ac130e6051f990f1cc8a461710e9d16555d7262b
Jimmytran16 <jimmytran1620@gmail.com>
2020-03-30 02:16:21 -0400

    added country codes

--- weather/views.py
+++ weather/views.py
@@ -1,35 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import pycountry
 from configs import API_KEY
 from .temp import Temp
-
-
-
+from pprint import pprint
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    list_alpha2 = []
+    for i in range(0,249):
+        list_alpha2.append(list(pycountry.countries)[i].alpha_2) # Get the list of the Alpha-2 Country codes and populate to the list
+    context = {
+        'alpha':list_alpha2
+    }
+    return render(request, 'index.html',context) # pass the list into the webpage
+
 
 def check(request):
     # get API key
     APIKEY = API_KEY
-    zip = request.GET['zip'] # get zip from the client
+    zip = request.GET['zip']  # get zip from the client
+    code = request.GET['countrycode']
     data_list = []
     # link to send a request to the API
     # url contains the zip code (US BASED) and developer API key
-    url = f'https://api.openweathermap.org/data/2.5/weather?zip={zip},us&appid={APIKEY}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?zip={zip},{code}&appid={APIKEY}'
+    print(url)
     # get the response from server --- convert in json file
     data = requests.get(url).json()
-    temp = Temp() # create an instance of the Temp class to use the KtoF function
+    if data['cod'] == '404':
+        return render(request,'index.html',{'err':'City not found!'}) # if city does not exist, send error to the index page
+
+    temp = Temp()  # create an instance of the Temp class to use the KtoF function
+    pprint(data)
     data_list.append(data['name'])
     data_list.append(temp.KtoF(data['main']['temp']))
-    data_list.append(data['sys']['country'])
+    data_list.append(zip)
     data_list.append(data['weather'][0]['description'])
+    data_list.append(data['sys']['country'])
 
     context = {
-        'data_list':data_list
+        'data_list': data_list
     }
     print(data_list[0])
-    return render(request,'weather.html',context) 
+    return render(request, 'weather.html', context)
